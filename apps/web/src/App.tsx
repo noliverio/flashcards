a1af9e941a55c9bdc56d04cd9c77756e5aa0764b
Nick Oliverio <nick@nicholasoliverio.com>
2026-05-26 20:54:46 -0400

    web: add Card ID input and Fetch button to load arbitrary cards

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,43 +1,63 @@
 import CardUI from './ui/card'
 import './App.css'
 import { getCard } from './lib/api-queries'
-import { useEffect, useState } from 'react'
-import { type selectCardSchema } from '@flashcards/database/schema'
+import { useEffect, useState, useRef } from 'react'
+import { selectCardSchema } from '@flashcards/database/schema'
 import { z } from "zod"
 
 type cardType = z.infer<typeof selectCardSchema>
 
 export function App() {
-  const [card, setCard] = useState<cardType>()
+  const [card, setCard] = useState<cardType | undefined>()
+  const [cardIDInput, setCardIDInput] = useState<string>('1')
+  const mountedRef = useRef(true)
 
-  useEffect(()=>{
-    async function _ (){
-      const eCard = await getCard(1)
-      
-      if (! ignore) { 
-        setCard(eCard)
-      }
+  const fetchCardById = async (id: number) => {
+    const eCard = await getCard(id)
+    if (mountedRef.current) {
+      setCard(eCard)
     }
+  }
 
-    let ignore = false
-    _()
-    return ()=>{
-      ignore = true
+  useEffect(() => {
+    fetchCardById(1)
+    return () => {
+      mountedRef.current = false
     }
   }, [])
 
-  console.log(1)
-  if (!card) {
-    return (
-      <div>
-        <p>Loading...</p>
-      </div>
-    )
-  }
-  console.log(2)
   return (
     <div>
-    <CardUI card={card} startWithQuestion={true}></CardUI>
+      <div style={{ marginBottom: '1rem' }}>
+        <label>
+          Card ID:&nbsp;
+          <input
+            type="number"
+            value={cardIDInput}
+            onChange={(e) => setCardIDInput(e.target.value)}
+          />
+        </label>
+        <button
+          onClick={() => {
+            const id = parseInt(cardIDInput, 10)
+            if (!Number.isNaN(id)) {
+              void fetchCardById(id)
+            }
+          }}
+        >
+          Fetch
+        </button>
+      </div>
+
+      {!card ? (
+        <div>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        <div>
+          <CardUI card={card} startWithQuestion={true} />
+        </div>
+      )}
     </div>
   )
 }
